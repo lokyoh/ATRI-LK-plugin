22b1515fe9f32705c702452ac9b12c6c02a820cc
l_o_o_k <2366360924@qq.com>
2024-09-17 14:02:22 +0800

    舔狗日志的更新

--- plugins/tiangou.py
+++ plugins/tiangou.py
@@ -6,7 +6,7 @@ from ATRI.service import Service
 
 DATA_PATH = TEXT_DIR / "tiangou.txt"
 
-tian_gou = Service("舔狗日记").document("爱你无需多言(doge)").type(Service.ServiceType.ENTERTAINMENT).version("1.0.0")
+tian_gou = Service("舔狗日记").document("爱你无需多言(doge)").type(Service.ServiceType.ENTERTAINMENT).version("1.1.0")
 
 get_tiangou = tian_gou.on_command(cmd='舔狗日记', docs='爱你无需多言(doge)')
 
@@ -18,12 +18,8 @@ async def _():
 
 tiangou = []
 
+with open(DATA_PATH, 'r', encoding='utf-8') as file:
+    for line in file:
+        tiangou.append(line)
 
-def load_tiangou():
-    with open(DATA_PATH, 'r', encoding='utf-8') as file:
-        for line in file:
-            tiangou.append(line)
-    log.success(f'舔狗日记共加载{len(tiangou)}个')
-
-
-driver().on_startup(load_tiangou)
+driver().on_startup(lambda: log.success(f'舔狗日记共加载{len(tiangou)}个'))
